b12aed92bec33b242196b6273e87ad0e767a642a
Greg Anders <greg@gpanders.com>
2018-10-27 09:26:44 -0600

    Use true colors

--- colors.zsh
+++ colors.zsh
@@ -1,4 +1,4 @@
 typeset -A fzf_colors
 
-fzf_colors["Solarized Dark"]="fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33,info:136,prompt:136,pointer:230,marker:230,spinner:136"
-fzf_colors["Solarized Light"]="fg:-1,bg:-1,hl:33,fg+:235,bg+:254,hl+:33,info:136,prompt:136,pointer:234,marker:234,spinner:136"
+fzf_colors["Solarized Dark"]="fg:-1,bg:-1,hl:#268bd2,fg+:#93a1a1,bg+:#073642,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
+fzf_colors["Solarized Light"]="fg:-1,bg:-1,hl:#268bd2,fg+:#586e75,bg+:#eee8d5,hl+:#268bd2,info:#b58900,prompt:#b58900,pointer:#2aa198,marker:#2aa198,spinner:#b58900"
